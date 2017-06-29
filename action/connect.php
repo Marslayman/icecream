ded759f3577f8fe93df9850819994dfcf576d34c
willxiao02 <zhangwenxiao1010@qq.com>
2017-06-29 16:13:08 +0800

    add connect pgm
    v0.1a

---
+++ action/connect.php
@@ -0,0 +1,18 @@
+﻿<?php
+$host="localhost";
+$db_user="   ";
+$db_pass="  ";
+$db_name="  ";
+$timezone="Asia/Shanghai";
+
+$link=mysqli_connect($host,$db_user,$db_pass);
+if(!$link)
+{
+	die("连接失败".mysqli_error($link));
+}
+mysqli_select_db($link,$db_name);
+mysqli_query($link,"SET names UTF8");
+
+header("Content-Type: text/html; charset=utf-8");
+date_default_timezone_set($timezone); //北京时间
+?>
